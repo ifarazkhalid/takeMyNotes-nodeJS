6cf51b8c4c5dfae8a8f9870e0754b828152e585c
Faraz Khalid <ifarazkhalid@gmail.com>
2018-05-06 21:56:18 -0500

    remove function updated
    Remove function has now been updated so that if a command with --title is passed, it will remove the object from JSON.

--- app.js
+++ app.js
@@ -13,13 +13,25 @@ const argv = yargs.argv;
 var commandName = process.argv[2];
 
 if (commandName === 'add') {
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+
+  if (note) {
+    console.log("Note created");
+    console.log("----");
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  } else {
+    console.log("Note title already taken.")
+  }
+
 } else if (commandName === 'list') {
   notes.getAll();
 } else if (commandName === 'read') {
   notes.getNote(argv.title);
 } else if (commandName === 'remove') {
-  notes.removeNote(argv.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note not found!'
+  console.log(message);
 } else {
   console.log('Command entered was not recognized');
 }
--- notes.js
+++ notes.js
@@ -3,24 +3,30 @@ console.log("Starting notes.js");
 // loading fs
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e){
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 // DESCRIPTION OF ADDNOTE
 // Add note function that adds note in the array
 // notes array that has all the notes. note variable stores the single note sent by user.
 // try-catch to check if file exists. if it does, load all notes in notes array.
 // checking for duplicateNotes
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     title,
     body
   };
-
-try {
-  var notesString = fs.readFileSync('notes-data.json');
-  notes = JSON.parse(notesString);
-} catch (e){
-
-}
 
 // checks for duplicateNotes
 var duplicateNotes = notes.filter((note) => {
@@ -29,7 +35,8 @@ var duplicateNotes = notes.filter((note) => {
 
 if (duplicateNotes.length === 0) {
   notes.push(note);
-  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  saveNotes(notes);
+  return note;
 }
 
 };
@@ -42,8 +49,13 @@ var getNote = () => {
   console.log("LOLOL note");
 };
 
-var removeNote = () => {
-  console.log("Removing note");
+var removeNote = (title) => {
+  var notes = fetchNotes();
+  // console.log(notes);
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
+
+return notes.length !== filteredNotes.lengths;
 };
 
 module.exports = {
